a0def08c43b8308eed61073a14553008dcd63592
albin5551 <albinantony@IN-15.94>
2022-11-28 18:22:37 +0530

    product add admin completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { CategoryComponent } from './admin/category/category.component';
+import { ProductComponent } from './admin/product/product.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+  {path: 'admin', component: AdminHomeComponent},
+  { path: 'category', component: CategoryComponent },
+  {path:'product',component:ProductComponent}
+   
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule,HTTP_INTERCEPTORS} from "@angular/common/http"
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { TokenServiceService } from './service/token-service.service';
+import { HomeComponent } from './home/home.component';
+
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { CategoryComponent } from './admin/category/category.component';
+import { ProductComponent } from './admin/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AdminHomeComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass:TokenServiceService,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
